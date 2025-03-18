4b94896c0cc4cea5c989110c6d985afa4641402f
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-03-18 18:41:25 +0100

    for now gone

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -130,30 +130,6 @@ function Footer() {
             Copyright 2025 - Auto Boomgaard
           </div>
           
-          <div className="footer-download-app">
-            <div className="footer-download-app-title">Download de occasion app</div>
-            <div className="app-links">
-              <a href="https://itunes.apple.com/nl/app/vd-akker/id1073901305?mt=8" target="_blank" rel="noopener noreferrer">
-                <i className="svg-icon svg-icon-apple">
-                  {/* SVG Apple Icon */}
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fff">
-                    <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
-                  </svg>
-                </i>
-                <span>Apple</span>
-              </a>
-              <a href="https://play.google.com/store/apps/details?id=nl.twisted.vdakkerapp" target="_blank" rel="noopener noreferrer">
-                <i className="svg-icon svg-icon-android">
-                  {/* SVG Android Icon */}
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fff">
-                    <path d="M17.6 9.48l1.84-3.18c.16-.31.04-.69-.26-.85-.29-.15-.65-.06-.83.22l-1.88 3.24c-2.86-1.21-6.08-1.21-8.94 0L5.65 5.67c-.19-.29-.58-.38-.87-.2-.28.18-.37.54-.22.83L6.4 9.48C3.3 11.25 1.28 14.44 1 18h22c-.28-3.56-2.3-6.75-5.4-8.52zM7 15.25c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25zm10 0c-.69 0-1.25-.56-1.25-1.25s.56-1.25 1.25-1.25 1.25.56 1.25 1.25-.56 1.25-1.25 1.25z"/>
-                  </svg>
-                </i>
-                <span>Android</span>
-              </a>
-            </div>
-          </div>
-
           <div className="social-media-links">
             <a href="https://www.facebook.com/pages/Vd-Akker/243312935679742" title="Facebook" target="_blank" rel="noopener noreferrer">
               <i className="svg-icon svg-icon-facebook">
