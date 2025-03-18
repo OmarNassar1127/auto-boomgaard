52e6766a4d0f93ee7cb965caf60e8ba70015131c
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-03-18 20:12:43 +0100

    some fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -131,26 +131,6 @@ function Header({ onNavChange, currentPage }) {
                   <span className="label">CONTACT</span>
                 </a>
               </li>
-              <li className="menu-item-search menu-item-icon">
-                <a href="/aanbod/" onClick={(e) => handleNavigation('search', e)}>
-                  <i className="svg-icon svg-icon-search">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <circle cx="11" cy="11" r="8"></circle>
-                      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                    </svg>
-                  </i>
-                </a>
-              </li>
               <li className="menu-item-menu menu-item-icon">
                 <label
                   htmlFor="header-nav-checkbox"
