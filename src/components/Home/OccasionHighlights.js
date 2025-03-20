dd28c5b3b35a3de04ed84fb79da6baa22ad0956e
Omar Nassar <omar.portero.nassar1@gmail.com>
2025-03-20 21:22:21 +0100

    done

--- src/components/Home/OccasionHighlights.js
+++ src/components/Home/OccasionHighlights.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Card, Row, Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./OccasionHighlights.css";
 
 function OccasionHighlights() {
@@ -110,7 +111,7 @@ function OccasionHighlights() {
                   onMouseLeave={handleMouseLeave}
                 >
                   <div className="card-image-wrapper">
-                    <div className="main-image-container">
+                    <div className="">
                       <img
                         src={occasion.images[activeImageIndex[occasion.id]]}
                         alt={`${occasion.brand} ${occasion.model}`}
@@ -160,15 +161,15 @@ function OccasionHighlights() {
                       </div>
                     </div>
                     
-                    <a 
-                      href={`https://www.akker.nl/aanbod/occasion/${occasion.id}/${occasion.brand.toLowerCase()}-${occasion.model.toLowerCase()}/`}
+                    <Link 
+                      to={`/auto/${occasion.id}`}
                       className="view-button"
                     >
                       <span>Bekijk</span>
                       <svg className="arrow-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/>
                       </svg>
-                    </a>
+                    </Link>
                   </div>
                 </div>
               </Col>
@@ -177,12 +178,12 @@ function OccasionHighlights() {
         )}
         
         <div className="section-footer">
-          <a href="/aanbod/" className="view-all-button">
+          <Link to="/aanbod/" className="view-all-button">
             Bekijk alle occasions
             <svg className="arrow-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"/>
             </svg>
-          </a>
+          </Link>
         </div>
       </Container>
     </section>
